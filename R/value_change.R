467c9594e46ec8b9fc75910197d616ff0a85f2fb
Mark Sendak <mark.sendak@gmail.com>
2017-11-02 08:49:23 +0000

    version 0.0.1

---
+++ R/value_change.R
@@ -0,0 +1,201 @@
+#' Identify changes in a value over time
+#'
+#' A function that reads in a time series data frame along with a specified
+#'  value change and identifies instances where the value change occurs.
+#' The user must specify the number of hours over which the value change must
+#'  take place, the magnitude and direction of the value change, a variable to
+#'  use to join the table to itself, the time stamp variable, and the value
+#'  variable. The user must also specify whether to keep all instances that
+#'  the value change occurs, or only the first or last instance.
+#' This function must be used carefully, because certain types of arguments
+#'  will cause the function to output a data frame with nrow(data)^2, where
+#'  'data' is the input data. More specifically, if the user is trying to
+#'  detect small variations in a value over a large period of time, the size
+#'  of input 'data' should be limited.
+#'
+#' @param data A time series data frame that includes the columns 'join_key',
+#'  'time_var', and 'value_var'
+#' @param value A numeric value specifying the magnitude of change to identify
+#' @param direction A string value specifying whether to identify changes in
+#'  the value up (an increase), down (a decrease), or all (both). The default
+#'  value is all.
+#' @param window_hours A numeric value specifying the number of hours to search
+#'  for the value change
+#' @param join_key A string name of the column to join the time series data
+#'  frame to itself. In other words, the primary key to the 'data' argument.
+#' @param time_var A string name of the time stamp column in the time series
+#'  data frame
+#' @param value_var A string name of the value variable column in the time
+#'  series data frame
+#' @param mult A string specifying whether to return the first, last, or all
+#'  instance(s) of the value change with a default value of all
+#'
+#' @return A data.frame, data.table with time stamps of value changes over time
+#'  along with values and time stamps for prior measurements
+#'
+#' @section Imported functions:
+#' fastPOSIXct() from fasttime package, foverlaps() from data.table and
+#'  general data.table syntax
+#'
+#' @section Errors:
+#' This function returns errors for:
+#' \itemize{
+#'  \item missing arguments (no arguments have defaults)
+#'  \item passing an invalid direction or mult value
+#'  \item passing arguments with invalid classes (data must be a data frame,
+#'    value must be numeric, and window_hours must be numeric)
+#'  \item passing join_key, time_var, or value_var values that are not column
+#'    names in input data frames
+#' }
+#'
+#' @examples
+#' library(data.table)
+#' systolic_bp <- as.data.table(vitals[VARIABLE == "SYSTOLIC_BP"])
+#'
+#' # Identify all instances of a drop of 40 over 6 hours
+#' value_change(systolic_bp, value = 40, direction = "down", window_hours = 6,
+#'  join_key = "PAT_ID", time_var = "RECORDED_TIME",
+#'  value_var = "VALUE", mult = "all")
+#' # Identify first instance of a drop of 40 over 6 hours
+#' value_change(systolic_bp, value = 40, direction = "down", window_hours = 6,
+#'  join_key = "PAT_ID", time_var = "RECORDED_TIME",
+#'  value_var = "VALUE", mult = "first")
+#' # Identify last instance of a drop of 40 over 6 hours
+#' value_change(systolic_bp, value = 40, direction = "down", window_hours = 6,
+#'  join_key = "PAT_ID", time_var = "RECORDED_TIME",
+#'  value_var = "VALUE", mult = "last")
+#' # Identify all instances of drops and increases of 40 over 6 hours
+#' value_change(systolic_bp, value = 40, direction = "all", window_hours = 6,
+#'  join_key = "PAT_ID", time_var = "RECORDED_TIME",
+#'  value_var = "VALUE", mult = "all")
+#'
+#' @export
+
+value_change <- function(data, value, direction = c("all", "up", "down"),
+  window_hours, join_key, time_var, value_var, mult = c("all", "first",
+  "last")) {
+
+  ########## Error handling ---------------------------------------------------
+  # Missing arguments
+  if (missing(data)) stop("Need to pass data frame")
+  if (missing(value)) stop("Need to specify change value")
+  if (missing(window_hours)) stop("Need to specify window hours")
+  if (missing(join_key)) stop("Need to specify join key")
+  if (missing(time_var)) stop("Need to specify time variable")
+  if (missing(value_var)) stop("Need to specify value variable")
+
+  # Direction and mult not from set of options
+  direction <- match.arg(direction)
+  mult <- match.arg(mult)
+
+  # Confirm argument classes
+  if (!is.data.frame(data)) stop("'data' must be a data.frame")
+  if (!is.numeric(value)) stop("'value' must be numeric")
+  if (!is.numeric(window_hours)) stop("'window_hours' must be numeric")
+
+  # Ensure join_key, time_var, and value_var are variables in data
+  if (sum(grepl(join_key, names(data))) == 0) {
+    stop("'join_key' is not a column name in data")
+  }
+  if (sum(grepl(time_var, names(data))) == 0) {
+    stop("'time_var' is not a column name in data")
+  }
+  if (sum(grepl(value_var, names(data))) == 0) {
+    stop("'value_var' is not a column name in data")
+  }
+
+  ########## Prep data for joins ----------------------------------------------
+  # Ensure classes
+  data <- data.table(data)
+  set(data, j = value_var, value = as.numeric(data[[value_var]]))
+  set(data, j = time_var, value = fastPOSIXct(data[[time_var]], tz = "GMT"))
+
+  # Subset data
+  data <- data[, c(join_key, time_var, value_var), with = FALSE]
+
+  # Upper case time_var, value_var
+  setnames(data, time_var, toupper(time_var))
+  time_var <- toupper(time_var)
+  setnames(data, value_var, toupper(value_var))
+  value_var <- toupper(value_var)
+
+  # Set keys
+  setkeyv(data, c(join_key, time_var))
+
+  # Subset to possible encounters that show necessary variation
+  V1 = NULL
+  event_subset <- data[, (max(get(value_var)) - min(get(value_var))),
+    by = join_key][V1 >= value][[join_key]]
+  data <- data[get(join_key) %in% event_subset]
+
+  # Make copy for join
+  prior_data <- copy(data)
+
+  # Normalize column names
+  setnames(data, c(time_var, value_var), c(paste0("CURRENT_", time_var),
+    paste0("CURRENT_", value_var)))
+  setnames(prior_data, c(time_var, value_var), c(paste0("PRIOR_", time_var),
+    paste0("PRIOR_", value_var)))
+
+  # Add end time to start table (end time = start time + window_hours)
+  prior_data[, paste0("CURRENT_", time_var) := (get(paste0("PRIOR_", time_var))
+   + (window_hours * 60 * 60))]
+  setcolorder(prior_data, c(join_key, paste0("PRIOR_", time_var),
+    paste0("CURRENT_", time_var), paste0("PRIOR_", value_var)))
+
+  # Add start time to end table (start time = end time)
+  data[, paste0("PRIOR_", time_var) := get(paste0("CURRENT_", time_var))]
+  setcolorder(data, c(join_key, paste0("PRIOR_", time_var),
+    paste0("CURRENT_", time_var), paste0("CURRENT_", value_var)))
+
+  # Set keys
+  setkeyv(data, c(join_key, paste0("PRIOR_", time_var),
+    paste0("CURRENT_", time_var)))
+  setkeyv(prior_data, c(join_key, paste0("PRIOR_", time_var),
+    paste0("CURRENT_", time_var)))
+
+  ########## Overlap join and post-process ------------------------------------
+  # Overlap join
+  join_data <- foverlaps(data, prior_data)
+
+  # Drop columns
+  join_data[,c(paste0("CURRENT_", time_var),
+    paste0("i.",paste0("PRIOR_", time_var))) := NULL]
+
+  # Fix column names
+  setnames(join_data, paste0("i.", paste0("CURRENT_", time_var)),
+    paste0("CURRENT_", time_var))
+
+  # Drop rows where measurement time are the same
+  join_data <- join_data[get(paste0("PRIOR_", time_var)) !=
+    get(paste0("CURRENT_", time_var))]
+
+  # Order by join_key and end time
+  setkeyv(join_data, c(join_key, paste0("CURRENT_", time_var)))
+
+  # Identify value changes
+  if (direction == "up") {
+    join_data <- join_data[get(paste0("PRIOR_", value_var)) -
+      get(paste0("CURRENT_", value_var)) <= - value]
+  }
+  else if (direction == "down") {
+    join_data <- join_data[get(paste0("PRIOR_", value_var)) -
+      get(paste0("CURRENT_", value_var)) >= value]
+  }
+  else {
+    join_data <- join_data[abs(get(paste0("PRIOR_", value_var)) -
+      get(paste0("CURRENT_", value_var))) >= value]
+  }
+
+  ########## Subset events ----------------------------------------------------
+  # Subset events
+  if (mult == "first") {
+    join_data <- join_data[, .SD[1], by = join_key]
+  }
+  else if (mult == "last") {
+    join_data <- join_data[, .SD[.N], by = join_key]
+  }
+
+  ########## Return table -----------------------------------------------------
+  return(join_data)
+}
---
+++ README.md
@@ -0,0 +1,134 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+constellation
+=============
+
+[![Build Status](https://travis-ci.org/marksendak/constellation.svg?branch=master)](https://travis-ci.org/marksendak/constellation) [![Windows build status](https://ci.appveyor.com/api/projects/status/github/marksendak/constellation?branch=master&svg=true)](https://ci.appveyor.com/project/marksendak/constellation)
+
+Overview
+--------
+
+Constellation contains a set of functions for applying multidimensional, time window based logic to time series data frames of arbitrary length. Constellation was developed to enable rapid and flexible identification of series of events that occur in hospitalized patients. The functions have been abstracted for general purpose use with time series data. Constellation extends and provides a friendly API to rolling joins and overlap joins implemented in [data.table](https://CRAN.R-project.org/package=data.table). Three datasets (labs, vitals, and orders) with randomly synthesized time series data for a cohort of 100 patients are included to facilitate testing of functions.
+
+There are three functions included in constellation to build complex features from time series data:
+
+-   `value_change()` identify increases or decreases in a value within a given time window
+-   `constellate()` identify time stamps when a series of events occur within a given time window
+-   `constellate_criteria()` identify which events occur within a given time window for every measurement time stamp
+
+Constellation can be used to build point-based scores for time series data, identify particular sequences of events that occur near each other, and identify when specific changes occur for a given parameter.
+
+If you are new to constellation, the best place to start is the `vignette("constellation", "identify_sepsis")`.
+
+Installation
+------------
+
+You can install constellation from github with:
+
+``` r
+devtools::install_github("marksendak/constellation")
+```
+
+This package is under development in preparation of release on CRAN. If you have any questions, comments, or feedback, please email <mark.sendak@gmail.com>.
+
+Example
+-------
+
+Below are several variations of finding systolic blood pressure drops of 40 over a 6 hour period.
+
+Examine systolic blood pressure data:
+
+``` r
+library(constellation)
+library(fasttime)
+
+systolic_bp <- vitals[VARIABLE == "SYSTOLIC_BP"]
+systolic_bp[, RECORDED_TIME := fastPOSIXct(RECORDED_TIME)]
+head(systolic_bp)
+#>    PAT_ID       RECORDED_TIME    VALUE    VARIABLE
+#> 1: 108546 2010-02-25 00:36:15 110.6677 SYSTOLIC_BP
+#> 2: 108546 2010-02-25 03:41:56 116.0423 SYSTOLIC_BP
+#> 3: 108546 2010-02-25 05:30:53 119.2235 SYSTOLIC_BP
+#> 4: 108546 2010-02-25 06:05:43 102.9899 SYSTOLIC_BP
+#> 5: 108546 2010-02-25 06:48:29 122.1348 SYSTOLIC_BP
+#> 6: 108546 2010-02-25 07:14:18 119.7529 SYSTOLIC_BP
+```
+
+Identify the first systolic blood pressure drop per patient:
+
+``` r
+systolic_bp_drop <- value_change(systolic_bp, value = 40, direction = "down",
+    window_hours = 6, join_key = "PAT_ID", time_var = "RECORDED_TIME", 
+    value_var = "VALUE", mult = "first")
+head(systolic_bp_drop)
+#>    PAT_ID PRIOR_RECORDED_TIME PRIOR_VALUE CURRENT_RECORDED_TIME
+#> 1: 108546 2010-02-25 10:45:29    139.9967   2010-02-25 15:42:35
+#> 2: 112374 2010-03-09 13:18:13    160.4919   2010-03-09 15:48:09
+#> 3: 113163 2010-07-27 11:50:35    170.2034   2010-07-27 15:21:58
+#> 4: 124042 2010-11-24 16:34:57    163.8912   2010-11-24 21:03:14
+#> 5: 135995 2010-11-20 20:51:09    157.9432   2010-11-20 22:26:00
+#> 6: 146478 2010-08-27 12:07:47    179.3603   2010-08-27 17:03:05
+#>    CURRENT_VALUE
+#> 1:      80.07446
+#> 2:     107.87212
+#> 3:     116.22419
+#> 4:     116.66625
+#> 5:     111.55469
+#> 6:     132.99234
+```
+
+Identify the last systolic blood pressure drop per patient:
+
+``` r
+systolic_bp_drop <- value_change(systolic_bp, value = 40, direction = "down",
+    window_hours = 6, join_key = "PAT_ID", time_var = "RECORDED_TIME", 
+    value_var = "VALUE", mult = "last")
+head(systolic_bp_drop)
+#>    PAT_ID PRIOR_RECORDED_TIME PRIOR_VALUE CURRENT_RECORDED_TIME
+#> 1: 108546 2010-07-01 11:31:31    164.9851   2010-07-01 17:03:04
+#> 2: 112374 2010-03-15 11:15:53    164.1634   2010-03-15 13:30:06
+#> 3: 113163 2010-07-30 15:12:15    160.1682   2010-07-30 18:33:10
+#> 4: 124042 2010-12-04 13:34:18    167.2564   2010-12-04 17:46:57
+#> 5: 135995 2010-11-26 23:47:15    127.5603   2010-11-27 01:43:05
+#> 6: 146478 2010-09-03 11:14:43    182.1690   2010-09-03 12:18:28
+#>    CURRENT_VALUE
+#> 1:     114.67968
+#> 2:     115.95783
+#> 3:     111.89387
+#> 4:     118.81151
+#> 5:      81.90537
+#> 6:     138.28222
+```
+
+Identify all systolic blood pressure drops per patient:
+
+``` r
+systolic_bp_drop <- value_change(systolic_bp, value = 40, direction = "down",
+    window_hours = 6, join_key = "PAT_ID", time_var = "RECORDED_TIME", 
+    value_var = "VALUE", mult = "all")
+head(systolic_bp_drop)
+#>    PAT_ID PRIOR_RECORDED_TIME PRIOR_VALUE CURRENT_RECORDED_TIME
+#> 1: 108546 2010-02-25 10:45:29    139.9967   2010-02-25 15:42:35
+#> 2: 108546 2010-03-01 10:57:24    136.8654   2010-03-01 11:07:00
+#> 3: 108546 2010-03-02 14:59:20    129.0167   2010-03-02 19:46:35
+#> 4: 108546 2010-03-02 15:49:00    110.1830   2010-03-02 19:46:35
+#> 5: 108546 2010-03-03 19:18:41    137.8095   2010-03-03 23:23:54
+#> 6: 108546 2010-03-03 21:13:39    130.3280   2010-03-03 23:23:54
+#>    CURRENT_VALUE
+#> 1:      80.07446
+#> 2:      88.88972
+#> 3:      69.94551
+#> 4:      69.94551
+#> 5:      82.16874
+#> 6:      82.16874
+```
+
+Why constellation?
+------------------
+
+In clinical medicine, there are a subset of conditions that are defined by a sequence of related events that unfold over time. These conditions are described as a "*constellation of signs and symptoms*."
+
+Duke Institute for Health Innovation
+------------------------------------
+
+constellation was originally developed to support a machine learning project at the [Duke Institute for Health Innovation](http://www.dihi.org/) to predict sepsis.
